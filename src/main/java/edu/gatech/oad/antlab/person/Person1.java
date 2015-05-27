0a9a9a546e34dd49cf5900b3acfc4cee75f0acb9
Nick Liu <nliu41@gatech.edu>
2015-05-27 14:46:01 -0400

    Deleted extra files

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,7 +30,7 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  return input.substring(3, input.size() - 1) + input.substring(0, 2);
+	  return input.substring(3, input.length() - 1) + input.substring(0, 2);
 	}
 	
 	/**
