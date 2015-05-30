e103e4a8665466671eb4b33f219bedcdf72d271d
Son Nguyen <snguyen48@gatech.edu>
2015-05-29 23:12:03 -0400

    Added readme.p5.txt

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -78,8 +78,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   
                   //Person5 replace P5 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Son Nguyen");
+                  toPrint += p5.toString("903018161");
                   
                   //Person6 replace P6 with your name
                   //and gburdell6 with your gt id
