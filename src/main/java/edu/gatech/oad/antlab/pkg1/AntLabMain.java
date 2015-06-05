d54c9f307b15b244738dfe46a48542ce9281f484
dylanfloyd <dylan.floyd@gmail.com>
2015-06-04 22:50:17 -0400

    Update AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+                  Person4 p4 = new Person4("Dylan Floyd");
+                  toPrint += p4.toString("dfloyd7");
                   
                   //Person5 replace P5 with your name
                   //and gburdell5 with your gt id
